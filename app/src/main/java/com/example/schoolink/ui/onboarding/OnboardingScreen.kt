d53c1604bdbc3fc540877cba0f7c43aea8ae694a
Uros <uros.lazarevic.4906@metropolitan.ac.rs>
2024-11-06 18:27:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/schoolink/ui/onboarding/OnboardingScreen.kt
@@ -0,0 +1,16 @@
+package com.example.schoolink.ui.onboarding
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OnboardingScreen() {
+
+}
+
+@Preview
+@Composable
+private fun OnboardingScreenPreview() {
+    OnboardingScreen()
+}
