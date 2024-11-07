405f59b9f5be2f84f2132283f128c03cd3ffd52a
Uros <uros.lazarevic.4906@metropolitan.ac.rs>
2024-11-07 17:21:58 +0100

    Added onboarding

--- app/src/main/java/com/example/schoolink/MainActivity.kt
+++ app/src/main/java/com/example/schoolink/MainActivity.kt
@@ -1,47 +1,29 @@
 package com.example.schoolink
 
+import com.example.schoolink.ui.onboarding.OnboardingScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.schoolink.ui.theme.SchoolinkTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SchoolinkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                OnboardingScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SchoolinkTheme {
-        Greeting("Android")
+        OnboardingScreen()
     }
 }
